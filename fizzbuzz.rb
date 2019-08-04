1660e43808862653826c16bc27e8afb80b114012
Chandan Raj <chandanraj1919@gmail.com>
2019-08-04 13:59:20 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(arg)
+  if arg / 3
+    put "fizz"
+  elsif arg / 5
+    put "buzz"
+  elsif arg/3 && arg/5
+    put "fizzbuzz"
+  else
+    return nil
+
+  
