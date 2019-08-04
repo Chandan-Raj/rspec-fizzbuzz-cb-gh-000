a65df6cd77b0cd339915bad9a21e820adad4c44d
Chandan Raj <chandanraj1919@gmail.com>
2019-08-04 14:08:09 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,7 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(arg)
   if(arg/3 == 0)
-    return "Fizz"
-  elsif(arg/5 == 0)
-    return "Buzz"
-  elsif(arg/3==0 && arg/5==0)
-    return "FizzBuzz"
-  else
-    return nil
+    "Fizz"
+
   end
 end
